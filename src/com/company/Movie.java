dc32f11c45c45d20c5f73ba9373a8b98346493c5
hayasilin <harvey.hayasi@gmail.com>
2020-11-21 10:53:38 +0800

    no need switch if we just pass Price object into Movie class

--- src/com/company/Movie.java
+++ src/com/company/Movie.java
@@ -8,29 +8,13 @@ public class Movie {
     private String _title;
     private Price _price;
 
-    public Movie(String title, int priceCode) {
+    public Movie(String title, Price price) {
         _title = title;
-        setPriceCode(priceCode);
+        _price = price;
     }
 
     public int getPriceCode() {
         return _price.getPriceCode();
-    }
-
-    public void setPriceCode(int arg) {
-        switch (arg) {
-            case REGULAR:
-                _price = new RegularPrice();
-                break;
-            case CHILDREN:
-                _price = new ChildrenPrice();
-                break;
-            case NEW_RELEASE:
-                _price = new NewReleasePrice();
-                break;
-            default:
-                throw new IllegalArgumentException("Incorrect Price Code");
-        }
     }
 
     public String getTitle() {
