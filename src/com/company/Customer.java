a08011bbed1243989384811f4ac35210a36f0c68
hayasilin <harvey.hayasi@gmail.com>
2020-11-21 10:07:07 +0800

    Use replace temp with query to replace totalAmount with a query method

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -20,7 +20,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0; // 總消費金額
         int frequentRenterPoints = 0; // 常客積點
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
@@ -32,12 +31,21 @@ public class Customer {
 
             // Show figures for this rental 顯示此筆租借資料
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
 
         // Add footer lines
-        result += "Ammount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Ammount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement(); // 取得一筆租借紀錄
+            result += each.getCharge();
+        }
         return result;
     }
 }
