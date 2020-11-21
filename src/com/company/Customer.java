42fa8bd89b33b27c4b2dd6624c3fece5a4ea6d07
hayasilin <harvey.hayasi@gmail.com>
2020-11-21 09:56:31 +0800

    movie use rental instance method into Rental class

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -50,31 +50,6 @@ public class Customer {
     }
 
     private double amountFor(Rental aRental) {
-        double result = 0;
-
-        // Determine amounts for each line
-        switch (aRental.getMovie().getPriceCode()) { // 取得影片出租價格
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2) {
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-
-            case Movie.NEW_RELEASE:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3) {
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+        return aRental.getCharge();
     }
 }
--- src/com/company/Rental.java
+++ src/com/company/Rental.java
@@ -16,4 +16,33 @@ public class Rental {
     public Movie getMovie() {
        return _movie;
     }
+
+    public double getCharge() {
+        double result = 0;
+
+        // Determine amounts for each line
+        switch (getMovie().getPriceCode()) { // 取得影片出租價格
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2) {
+                    result += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+
+            case Movie.NEW_RELEASE:
+                result += 1.5;
+                if (getDaysRented() > 3) {
+                    result += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (getDaysRented() > 3) {
+                    result += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
 }
