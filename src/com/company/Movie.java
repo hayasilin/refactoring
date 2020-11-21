40c6cc8666809624b444547fee7cfc54d2261762
hayasilin <harvey.hayasi@gmail.com>
2020-11-21 10:29:02 +0800

    Move getCharge() logic from Movie class to Price class

--- src/com/company/Movie.java
+++ src/com/company/Movie.java
@@ -38,32 +38,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double result = 0;
-
-        // Determine amounts for each line
-        switch (getPriceCode()) { // 取得影片出租價格
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-
-            case Movie.NEW_RELEASE:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-
-            case Movie.CHILDREN:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
--- src/com/company/Price.java
+++ src/com/company/Price.java
@@ -2,11 +2,19 @@ package com.company;
 
 abstract class Price {
     abstract int getPriceCode();
+    abstract double getCharge(int daysRented);
 }
 
 class ChildrenPrice extends Price {
     int getPriceCode() {
         return Movie.CHILDREN;
+    }
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3) {
+            result += (daysRented - 3) * 1.5;
+        }
+        return result;
     }
 }
 
@@ -14,10 +22,21 @@ class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
 
 class RegularPrice extends Price {
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+    public double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2) {
+            result += (daysRented - 2) * 1.5;
+        }
+        return result;
     }
 }
